241c14eb4cc241174c6bf8367decfe9bc4c95fb8
jtrew89 <52958532+jtrew89@users.noreply.github.com>
2024-06-05 16:51:47 +0100

    Update cg_mlst_dist.py
    update

--- cg_mlst_dist.py
+++ cg_mlst_dist.py
@@ -6,8 +6,7 @@ import argparse
 
 def main(args):
 	##load data
-	alle_prof_df = pd.read_table('C:/Users/m1011068/OneDrive - Defra/Documents/Publications/Infantis for Shaun/New_report_tree/all_results_alleles_form_new.tsv').set_index('FILE')
-	#alle_prof_df = pd.read_csv('C:/Users/m1011068/OneDrive - Defra/Documents/Publications/Infantis for Shaun/New_report_tree/test_profile.csv').set_index('FILE')
+	alle_prof_df = pd.read_table(args['pro_db']).set_index('FILE')
 	
 	##Variables used in script
 	isolates = list(alle_prof_df.index)
@@ -36,26 +35,22 @@ def main(args):
 	        out_alle_df.at[isolate_1, isolate_2] = dist
 	
 	
-	#final_alle_df.to_csv('C:/Users/m1011068/OneDrive - Defra/Documents/Publications/Infantis for Shaun/new_report_tree/distance_mat.tsv',sep='\t')
+	final_alle_df.to_csv(args['out_dir'], sep='\t', index=True)
  
 if __name__=='__main__':
 	##Create arguments
-	parser = argparse.ArgumentParser(description='Script written to identify INF alleles from results_alleles.tsv output from Chewbbaca and to mark all novel allele designations within results_alleles.tsv')
+	parser = argparse.ArgumentParser(description='Script written to create distance matrix from results_alleles.tsv, of some other tab-delimeted allele profile')
+	
 	parser.add_argument(
-		'-id', '--input_directory',
-		dest='in_dir',
-		help="Directory where results_alleles.tsv is kept",
-		required=True
-                )
-
+	'-pdb', '--profile_db', dest='pro_db',
+	help='Alelle profile db with isolates you wish analysed (Include path)',
+	required=True
+	)
 	parser.add_argument(
-		'-od', '--output_directory',
-		dest='out_dir',
-		help="""Directory you want the output INF
-		list and the formated results_alleles.tsv
-		(results_alleles_form.tsv) to be output""",
-		required=True
-		)
+	'-od', '--output_dir', dest='out_dir',
+	help='Directory to output matrix',
+	required=True
+	)
 	args = parser.parse_args()
 
 	##Convert the argparse.Namespace to a dictionary: vars(args)
