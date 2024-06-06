318f370d3476b2279b0745718f6b0ac2982601e5
jtrew89 <52958532+jtrew89@users.noreply.github.com>
2024-06-06 16:27:10 +0100

    Update cg_mlst_dist.py
    update-formatting

--- cg_mlst_dist.py
+++ cg_mlst_dist.py
@@ -33,21 +33,21 @@ def main(args):
 	if args['miss']:
 		for isolate_1 in isolates:
 	    
-		    for isolate_2 in isolates:
-			dist = len(alle_prof_df.loc[isolate_1].compare(alle_prof_df.loc[isolate_2]).replace(0.0,np.nan).dropna()) #drops missing data per-pairwise comparison
-		        out_alle_df.at[isolate_1, isolate_2] = dist
+			for isolate_2 in isolates:
+				dist = len(alle_prof_df.loc[isolate_1].compare(alle_prof_df.loc[isolate_2]).replace(0.0,np.nan).dropna()) #drops missing data per-pairwise comparison
+		        	out_alle_df.at[isolate_1, isolate_2] = dist
 		
 		
-		final_alle_df.to_csv(args['out_dir'], sep='\t', index=True)
+			final_alle_df.to_csv(args['out_dir'], sep='\t', index=True)
 	else:
 		for isolate_1 in isolates:
 		    
-		    for isolate_2 in isolates:
-		        dist = len(alle_prof_df.loc[isolate_1].compare(alle_prof_df.loc[isolate_2])) #keeps missing data
-		        out_alle_df.at[isolate_1, isolate_2] = dist
+			for isolate_2 in isolates:
+		        	dist = len(alle_prof_df.loc[isolate_1].compare(alle_prof_df.loc[isolate_2])) #keeps missing data
+		        	out_alle_df.at[isolate_1, isolate_2] = dist
 		
 		
-		final_alle_df.to_csv(args['out_dir'], sep='\t', index=True)
+			final_alle_df.to_csv(args['out_dir'], sep='\t', index=True)
  
 if __name__=='__main__':
 	##Create arguments
